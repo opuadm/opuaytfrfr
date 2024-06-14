7f99a85511febd0c610b66bd3de3e04c8a4fc348
opuadm <79022132+opuadm@users.noreply.github.com>
2024-06-14 21:38:56 +0200

    Update draggables.js

--- assets/main/draggables.js
+++ assets/main/draggables.js
@@ -173,8 +173,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
         const windowHeight = window.innerHeight;
 
         // Set initial position in pixels
-        let initialLeft = windowWidth - 24 - element.offsetWidth; // 486px from the right
-        let initialTop = windowHeight - 35 - element.offsetHeight;  // 211px from the bottom
+        let initialLeft = windowWidth - 521 - element.offsetWidth; // 486px from the right
+        let initialTop = windowHeight - 232 - element.offsetHeight;  // 211px from the bottom
 
         // Ensure the element stays within the bounds on load
         if (initialLeft + element.offsetWidth > windowWidth) {
