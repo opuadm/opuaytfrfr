3148ff113f3fe105f1f111b550a56328083ebd8b
opuadm <79022132+opuadm@users.noreply.github.com>
2024-06-19 00:47:56 +0200

    Update start-menu.js

--- assets/main/start-menu.js
+++ assets/main/start-menu.js
@@ -2,11 +2,18 @@ document.addEventListener('DOMContentLoaded', function () {
     const startButton = document.getElementById('start-button');
     const startMenu = document.getElementById('start-menu');
 
-    startButton.addEventListener('click', function () {
-        if (startMenu.style.display === 'none' || startMenu.style.display === '') {
-            startMenu.style.display = 'block';
-        } else {
-            startMenu.style.display = 'none';
-        }
-    });
+    if (startButton && startMenu) {
+        console.log('Elements found');
+        
+        startButton.addEventListener('click', function () {
+            console.log('Start button clicked');
+            if (startMenu.style.display === 'none' || startMenu.style.display === '') {
+                startMenu.style.display = 'block';
+            } else {
+                startMenu.style.display = 'none';
+            }
+        });
+    } else {
+        console.log('Elements not found');
+    }
 });
