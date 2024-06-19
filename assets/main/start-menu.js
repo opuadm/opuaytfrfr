45e71c09d9becc534babf69cb4346a080488ebcf
opuadm <79022132+opuadm@users.noreply.github.com>
2024-06-19 17:37:52 +0200

    Update start-menu.js

--- assets/main/start-menu.js
+++ assets/main/start-menu.js
@@ -31,3 +31,37 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log('Elements not found');
     }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const startButton = document.querySelector('.toggle2');
+    const startMenu = document.querySelector('.taskbar');
+
+    if (startButton && startMenu) {
+        startButton.addEventListener('click', function () {
+            if (startMenu.style.display === 'none' || startMenu.style.display === '') {
+                startMenu.style.display = 'flex';
+            } else {
+                startMenu.style.display = 'none';
+            }
+        });
+    } else {
+        console.log('Elements not found');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const startButton = document.querySelector('.toggle2');
+    const startMenu = document.querySelector('.start-menu');
+
+    if (startButton && startMenu) {
+        startButton.addEventListener('click', function () {
+            if (startMenu.style.display === 'none' || startMenu.style.display === '') {
+                startMenu.style.display = 'block';
+            } else {
+                startMenu.style.display = 'none';
+            }
+        });
+    } else {
+        console.log('Elements not found');
+    }
+});
